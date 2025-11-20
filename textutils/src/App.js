bd03241956b4653b4d58ceeac2a7e38f383ceaad
Lancerfx <yt0328575@gmail.com>
2025-11-20 21:24:53 +0545

    js

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name = "Lancy";
 function App() {
   return (
     <>
@@ -9,6 +9,7 @@ function App() {
       <li>Product</li>
       <li>Contact</li>
     </nav>
+    <h1>Hello {name}</h1>
     <div className='Container'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi hic illum adipisci maxime! Corrupti blanditiis dolorem quod tempore rem iure minima beatae accusantium nemo deserunt, earum eum cum magnam quia inventore itaque tenetur!
     </div>
