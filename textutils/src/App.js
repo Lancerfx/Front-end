4d48996b32a53f244ede5557322df8bdf5f91975
Lancerfx <yt0328575@gmail.com>
2025-11-20 21:15:37 +0545

    learning

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="blank">Lovely</div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Product</li>
+      <li>Contact</li>
+    </nav>
+    <div className='Container'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi hic illum adipisci maxime! Corrupti blanditiis dolorem quod tempore rem iure minima beatae accusantium nemo deserunt, earum eum cum magnam quia inventore itaque tenetur!
+    </div>
+    </>
   );
 }
 
